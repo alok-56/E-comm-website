ab9adf2ced46567a56e4b2273b2f131c05d660d1
alokkumar <alokkumar11746@gmal.com>
2022-09-20 01:59:46 +0530

    make it better

--- index.js
+++ index.js
@@ -261,11 +261,11 @@ app.post('/adwin', async(req,res) => {
 
 //---------------heruku deployment--------------
 
-const dirname1=path.resolve();
+const __dirname1=path.resolve();
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"/client/build")));
+  app.use(express.static(path.join(__dirname1,"/client/build")));
   app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+    res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
   })
 }else{
   app.get("/",(req,res)=>{
