f9dc02c0edb065cd3cc04a1ffceae1623a17013b
alokkumar <alokkumar11746@gmal.com>
2022-09-20 00:45:33 +0530

    make it better

--- index.js
+++ index.js
@@ -248,10 +248,11 @@ app.post('/adwin', async(req,res) => {
 })
 
 //----------------heruku--------------
-if(process.env.NODE_ENV == "production"){
+if(process.env.NODE_ENV === "production"){
   app.use(express.static("./client/build"));
 };
 
+const path=require("path")
 app.get("*",(req,res)=>{
   res.sendFile(path.join(__dirname,'client','build','index.html'));
 })
