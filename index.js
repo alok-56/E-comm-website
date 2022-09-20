77a568c2dff0302d1bafbccf9a6a77ad5a624a9e
alokkumar <alokkumar11746@gmal.com>
2022-09-20 16:45:51 +0530

    make it better

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ cloudinary.config({
 });
 
 const mongoose=require("mongoose");
-const exp = require('constants');
 
 const db='mongodb+srv://alok56:ak56@cluster0.65jpmqq.mongodb.net/apnadukan?retryWrites=true&w=majority';
 mongoose.connect(db).then(()=>{
